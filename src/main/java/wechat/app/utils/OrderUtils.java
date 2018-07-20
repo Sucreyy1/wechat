d5f7f8d87000a5eafb3b01da53325f772c3d1205
liulingling600 <616748734@qq.com>
2018-07-20 19:37:34 +0800

    增加订单工具类

---
+++ src/main/java/wechat/app/utils/OrderUtils.java
@@ -0,0 +1,26 @@
+package wechat.app.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by 61674 on 2018/7/20.
+ */
+public class OrderUtils {
+    /**
+     * 获取订单号
+     * @return
+     */
+    public static String getOrderId() {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmss");
+        String newDate=sdf.format(new Date());
+        String result="";
+        Random random=new Random();
+        for(int i=0;i<6;i++){
+            result+=random.nextInt(10);
+        }
+        return newDate+result;
+    }
+
+}

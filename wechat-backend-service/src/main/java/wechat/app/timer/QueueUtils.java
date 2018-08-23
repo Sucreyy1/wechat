228f76d627fc8eed9adf85b4b491319243ac1192
Sucreyy1 <593143487@qq.com>
2018-08-23 17:11:33 +0800

    延时队列轮询（还未测试）

--- wechat-backend-service/src/main/java/wechat/app/timer/QueueUtils.java
+++ wechat-backend-service/src/main/java/wechat/app/timer/QueueUtils.java
@@ -2,22 +2,50 @@ package wechat.app.timer;
 
 import wechat.app.dao.entity.OrderInfo;
 
+import java.util.Date;
 import java.util.concurrent.DelayQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class QueueUtils {
 
     //Thread-safe queue
     private static DelayQueue<OrderInfo> queue = new DelayQueue<>();
 
+    private static Lock lock = new ReentrantLock();
+
+    private static Condition condition = lock.newCondition();
+
     private QueueUtils(){
 
     }
 
     public static void payStatusCheck(OrderInfo orderInfo){
-        queue.add(orderInfo);
+        if (queue.add(orderInfo)){
+            condition.signal();
+        }
     }
 
-    public static DelayQueue getQueue(){
-        return queue;
+
+    public void check(){
+        new Thread(()->{
+            try {
+                for (;;){
+                    OrderInfo orderInfo = queue.poll();
+                    if (orderInfo != null) {
+                        //超时将订单置为无效
+                        orderInfo.setStatus("");
+                        orderInfo.setUpdateTime(new Date());
+                    }
+                    else {
+                        condition.await();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
+
 }
